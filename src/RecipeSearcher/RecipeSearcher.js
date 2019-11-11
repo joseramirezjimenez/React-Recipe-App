e07823af374eb24cea28c78591fb7ef00e1592ce
joseramirezjimenez <jose.ramirez5019b@gmail.com>
2019-11-11 14:05:08 -0600

    Added in the pther two API calls and the default state

--- src/RecipeSearcher/RecipeSearcher.js
+++ src/RecipeSearcher/RecipeSearcher.js
@@ -3,23 +3,62 @@ import axios from 'axios';
 
 class RecipeSearcher extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {recipes: [] } 
+    }
+
     getRandomRecipe = () => {
         axios({
             /* We can configure everything we need to about the HTTP request in here */
             method: 'GET',
             url: 'https://www.themealdb.com/api/json/v1/1/random.php'
         })
-        .then(function(response) {
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
+    getRecipeByName = (name) => {
+        axios({
+            method: 'GET',
+            url: 'https://www.themealdb.com/api/json/v1/1/search.php',
+            params: {
+                s: name
+            }
+        })
+        .then(function(response){
             console.log(response);
         })
-        .catch(function(error) {
+        .catch(function(error){
+            console.log(error);
+        })
+    }
+
+    //Create a function called getRecipesByLetter that takes in a letter as a parameter. This should carry out the proper Axios call to the database. You will need to pass in the letter as the param f in the call
+
+    getRecipesByLetter = (letter) => {
+        axios({
+            method: 'GET',
+            url: 'https://www.themealdb.com/api/json/v1/1/search.php',
+            params: {
+                f: letter
+            }
+        })
+        .then(function(response){
+            console.log(response);
+        })
+        .catch(function(error){
             console.log(error);
         })
     }
 
     render() {
 
-        this.getRandomRecipe();
+        this.getRecipesByLetter('O');
 
         return (
             <div>
